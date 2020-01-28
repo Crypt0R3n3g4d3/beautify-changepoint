28305abf3d77879ebe9e9f775d3e2360511eded6
Dominic Cicilio <dominiccicilio@DCPPLTDCICILLIO.fios-router.home>
2020-01-27 20:06:33 -0500

    finished first iteration of dropdown buttons

--- js-modifications.js
+++ js-modifications.js
@@ -1,56 +1,112 @@
 
-let elementDropdownGeneral = document.querySelectorAll("#menuWrap #liMPERSONAL")[0];
-
-
-let elementDropdownGeneralLink = document.querySelectorAll("#menuWrap #lnkMPERSONAL")[0];
+// General DropDown
+const elementDropdownGeneralLink = document.querySelectorAll("#menuWrap #lnkMPERSONAL")[0];
 
 // Drop down Items
-let elementDropdownGeneralItems = document.querySelectorAll("#divMPERSONAL")[0];
+const elementDropdownGeneralMenu = document.querySelectorAll("#divMPERSONAL")[0];
 
 // Drop down Menu
-let elementDropdownGeneralMenu = document.querySelectorAll("#divMPERSONAL ul")[0];
+const elementDropdownGeneralMenuItems = document.querySelectorAll("#divMPERSONAL ul li");
 
 
-// Drop Down Link
 
-// Counts how many times the dropdown link onclick function has fired
-let toggledCount = 0;
+// Requests DropDown
+const elementDropdownRequestsLink = document.querySelectorAll("#menuWrap #lnkMSUPPORT")[0];
 
-elementDropdownGeneralLink.onclick = () => {
-	if (toggledCount === 0) {
-		console.log("Data Drop Down False");
-		elementDropdownGeneralItems.style = `transform: translateX(0px)`;
+// Drop down Items
+const elementDropdownRequestsMenu = document.querySelectorAll("#divMSUPPORT")[0];
+
+// Drop down Menu
+const elementDropdownRequestsMenuItems = document.querySelectorAll("#divMSUPPORT ul li");
+
+
+
+// Analytics DropDown
+const elementDropdownAnalyticsLink = document.querySelectorAll("#menuWrap #lnkMCOGNOS")[0];
+
+// Drop down Items
+const elementDropdownAnalyticsMenu = document.querySelectorAll("#divMCOGNOS")[0];
+
+// Drop down Menu
+const elementDropdownAnalyticsMenuItems = document.querySelectorAll("#divMCOGNOS ul li");
+
+
+// SETUP
+
+// Setup General Dropdown
+initDropDownButton(elementDropdownGeneralLink, elementDropdownGeneralMenu, elementDropdownGeneralMenuItems);
+
+// Setup Request Dropdown
+initDropDownButton(elementDropdownRequestsLink, elementDropdownRequestsMenu, elementDropdownRequestsMenuItems);
+
+// Setup Analytics Dropdown
+initDropDownButton(elementDropdownAnalyticsLink, elementDropdownAnalyticsMenu, elementDropdownAnalyticsMenuItems);
+
+/**
+ * Binds eventlisteners and CSS classes to elements;
+ * @param {HtmlElement} button - The dropdown button
+ * @param {HtmlElement} button - Container of dropdown content
+ * @param { Array of HtmlElement} menuItems - An array of each menu item
+ **/
+function initDropDownButton(button, menu, menuItems) {
+	// Drop Down Link
+		
+	button.classList.add('button');
+	button.innerHTML = `<span>${button.innerHTML}</span>`
+	
+	// elementDropdownGeneral.classList.add('button');
+	button.innerHTML += `<span class="icon is-small">
+	        <i class="fas fa-angle-down" aria-hidden="true"></i>
+	      </span>`;
+	
+	// Counts how many times the dropdown link onclick function has fired
+	let toggledCount = 0;
+	
+	button.onclick = () => {
+		console.log("dropdown click");
+		if (toggledCount === 0) {
+			console.log("False");
+			menu.style = `transform: translateX(0px)`;
+			button.setAttribute("data-toggle-dropdown", "true");
+			
+			// Switch Icon
+			const icon = button.querySelector('i');
+			icon.classList.remove("fa-angle-down");
+			icon.classList.add("fa-angle-up");
+		}
+		
+		toggledCount += 1;
+		return false;
 	}
 	
-	toggledCount += 1;
-	return false;
+	document.addEventListener('click', (e) => {
+		
+		console.log("doc click");
+		if (toggledCount > 1) {
+			console.log("data true")
+			menu.style = `transform: translateX(-1000px)`;
+			button.setAttribute("data-toggle-dropdown", "false");
+			
+			// Switch Icon
+			const icon = button.querySelector('i');
+			icon.classList.remove("fa-angle-up");
+			icon.classList.add("fa-angle-down");
+			
+			toggledCount = 0;
+		} else if (toggledCount === 1) {
+			toggledCount += 1;
+		}
+		
+	});
+
+	
+	
+	let menuitems = [...menuItems];
+	
+	console.log(menuitems);
+	
+	menuitems.forEach((item) => {
+		console.log(item);
+		item.classList.add("dropdown-item");
+	});
 }
-
-document.addEventListener('click', (e) => {
-	if (toggledCount > 1) {
-		console.log("Drop down data true")
-		elementDropdownGeneralItems.style = `transform: translateX(-1000px)`;
-		elementDropdownGeneralLink.setAttribute("data-toggle-dropdown", "0");
-		toggledCount = 0;
-	}
-});
-
-elementDropdownGeneralLink.classList.add('button');
-
-elementDropdownGeneralLink.innerHTML = `<span>${elementDropdownGeneralLink.innerHTML}</span>`
-
-
-
-// elementDropdownGeneral.classList.add('button');
-elementDropdownGeneralLink.innerHTML += `<span class="icon is-small">
-        <i class="fas fa-angle-down" aria-hidden="true"></i>
-      </span>`;
-      
-console.log(elementDropdownGeneralLink);
-      
-
-
-
-// dropdown-menu
-
-
